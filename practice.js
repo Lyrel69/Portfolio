6c9a882720c873aeb81054fd7839b2e139c3a968
Lyrel69 <lyrelbetuin@gmail.com>
2026-07-04 15:03:33 +0800

    first commit

--- practice.js
+++
@@ -1,6 +0,0 @@
-print("Hello!");
-print("My name is Christian Mark M. Doria."); 
-print("I am 20 years old.");
-print("I live in Cebu.");
-print("My favorite color is green.");
-print("I want to become a web developer.")
